03dee459239e72166becb4af08ab4e7aef62a625
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2017-11-30 13:40:19 -0500

    wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,50 @@ function getRndInteger(min, max)
 
 $(document).ready(function()
 	{
-    	$("#randomnumberbox").text(getRndInteger(19,120));
+		var losses = 0;
+		var wins = 0;
+		var store = getRndInteger(19,120);
+    	$("#randomnumberbox").text(store);
+    	
+    	//========================================================================================
 
+    	
+  		// Here we created a counter, we'll be using this to track the user's total.
+  		var counter = 0;
+
+  		// Here we created an on-click event that responds to button clicks of the crystal image.
+  		$(".crystal-image").on("click", function() {
+
+  		// Here we establish the "targetNumber" (set to getrndInteger between 1-12).
+  		var targetNumber = getRndInteger(1,12);
+  		console.log(targetNumber);
+
+   		 // Each time the user clicks the crystal the counter goes up by targetNumber which is getRndInteger between 1-12.
+    	counter += targetNumber;
+
+    	// We then output the number of times the crystal is clicked.
+    	$("#totalscore").text("Your score is increased by " + targetNumber + " so your new score is: " + counter);
+
+    	if (counter === store) {
+
+     	 // If the numbers match we'll celebrate the user's win.
+      		alert("You win!");
+      		$("#wins").text(wins++);
+
+   		 }
+
+   		 else if (counter> store)
+    	{
+    		// If the number is more than the genered number, we'll let the user know the loss
+    		alert("you lose!");
+    		$("#losses").text(losses++);
+    		
+    	}
+
+
+  });
+    	
     }
     
     );
+ 
