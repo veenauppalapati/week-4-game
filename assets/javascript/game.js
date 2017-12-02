f28256dc6dd5c51c1ce65bf92d6564c89836bbe9
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2017-12-01 23:56:21 -0500

    function reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,98 @@
+
+// Here we are creating a function that generates a random number
 function getRndInteger(min, max) 
-		{
-    		return Math.floor(Math.random() * (max - min + 1)) + min;
-		}
-		// console.log(getRndInteger(19,120));
-		
+
+    {
+
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    // console.log(getRndInteger(19,120));
+
+
+
+//===========================================================================================
+ 
+ // variable declarations   (GLOBAL)
 var losses = 1;
 var wins = 1;
 var counter = 0;
 var setToZero = 0;
-$(document).ready(function reset()
-	{
-		
-		var store = getRndInteger(19,120);
-    	$("#randomnumberbox").text(store);
-    	
-    	//========================================================================================
+var store =0;
 
-    	
-  		// Here we created a counter, we'll be using this to track the user's total.
-  		
+//===========================================================================================
 
-  		// Here we created an on-click event that responds to button clicks of the crystal image.
-  		$(".crystal-image").on("click", function() {
+function reset()
+  {
+    // Here we are declaring the variable store to store the value of generated integer between  19-120;
+    store = getRndInteger(19,120);
+    // Here in randomnumberbox div, we are calling the function getRndInteger to generate a number betweein 19-120
+    $("#randomnumberbox").text(store);
 
-  		// Here we establish the "targetNumber" (set to getrndInteger between 1-12).
-  		var targetNumber = getRndInteger(1,12);
-  		console.log(targetNumber);
+    counter = 0;
+    $("#totalscore").text("Your score is reset to: " + counter);
+    counter = counter + targetNumber;
+    $("#totalscore").text("Your score is increased by " + targetNumber + " so your new score is: " + counter);
 
-   		 // Each time the user clicks the crystal the counter goes up by targetNumber which is getRndInteger between 1-12.
-    	counter += targetNumber;
+  }
 
-    	// We then output the number of times the crystal is clicked.
-    	$("#totalscore").text("Your score is increased by " + targetNumber + " so your new score is: " + counter);
+//===========================================================================================
 
-    	if (counter === store) {
+$(document).ready(function ()
+  
+  {
 
-     	 // If the numbers match we'll celebrate the user's win.
-      		alert("You win!");
-      		$("#wins").text("Wins: " +wins++);
-          $("#totalscore").text("reset to: " + setToZero);
-          counter=set;
-          reset();
-   		 }
+    // Here we are declaring the variable store to store the value of generated integer between  19-120;
+    store = getRndInteger(19,120);
+    // Here in randomnumberbox div, we are calling the function getRndInteger to generate a number betweein 19-120
+    $("#randomnumberbox").text(store);
+      
+    //========================================================================================
 
-   		 else if (counter> store)
-    	{
-    		// If the number is more than the genered number, we'll let the user know the loss
-    		alert("you lose!");
-    		$("#losses").text("losses: " + losses++);
+    // Here we created an on-click event that responds to button clicks of the crystal image.
+    $(".crystal-image").on("click", function() 
+
+    {
+
+      // Here we establish the "targetNumber" (set to getrndInteger between 1-12).
+      var targetNumber = getRndInteger(1,12);
+      console.log(targetNumber);
+
+       // Each time the user clicks the crystal the counter goes up by targetNumber which is getRndInteger between 1-12.
+      counter = counter + targetNumber;//(counter = counter + targetNumber)
+
+      // We then output the number of times the crystal is clicked.
+      $("#totalscore").text("Your score is increased by " + targetNumber + " so your new score is: " + counter);
+
+      //-------------------------------------------------------------------------------------------------------
+
+      if (counter === store) 
+
+      {
+
+       // If the numbers match we'll celebrate the user's win.
+        alert("You win!");
+        $("#wins").text("Wins: " + wins++);
         $("#totalscore").text("reset to: " + setToZero);
-        counter=setToZero;
         reset();
+       
+      }
 
-    		
-    	}
+      else if (counter > store)
 
+      {
+        // If the number is more than the genered number, we'll let the user know the loss
+        alert("you lose!");
+        $("#losses").text("losses: " + losses++);
+        $("#totalscore").text("reset to: " + setToZero);
+        reset();
+        
+      }
 
+    //========================================================================================
+
+    });
+
+  //========================================================================================
+      
   });
-    	
-    }
-    
-    );
  
